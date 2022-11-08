da4cac5efc09a29833c48371e489fd3eb3a0fa6a
LeePMadden <79556356+LeePMadden@users.noreply.github.com>
2022-11-08 21:01:47 +1100

    Update collisionAvoidanceUR3.m
    notes to jtraj use

--- Assignment 2/collisionAvoidanceUR3.m
+++ Assignment 2/collisionAvoidanceUR3.m
@@ -236,7 +236,7 @@ steps = 2;
 while ~isempty(find(maxStepRadians < abs(diff(jtraj(q1,q2,steps))),1))
     steps = steps + 1;
 end
-qMatrix = jtraj(q1,q2,steps);
+qMatrix = jtraj(q1,q2,steps);           % step through q values using jtraj ~ JTRAJ USED HERE
 end
 
 %% InterpolateWaypointRadians
